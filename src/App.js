78514a2b27def338331ace5636f4c581505f0cc5
Yitshhaq Fukushima <ymotse@gmail.com>
2020-09-28 19:43:31 -0300

    Added Loading component when page is loading

--- src/App.js
+++ src/App.js
@@ -64,6 +64,12 @@ export default () => {
                 Direitos de imagem para Netflix <br/>
                 Dados pegos do site Themoviedb.org
             </footer>
+            
+            {movieList.length <= 0 &&
+                <div className="loading">
+                    <img src="https://www.filmelier.com/pt/br/news/wp-content/uploads/2020/03/netflix-loading.gif" alt="Carregando" />
+                </div>
+            }
         </div>
     )
 
