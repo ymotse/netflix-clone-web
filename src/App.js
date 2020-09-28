ae1804b89b4b2cf03997fe8b79bb5a4ce4080607
Yitshhaq Fukushima <ymotse@gmail.com>
2020-09-28 18:26:15 -0300

    Implemented FeaturedMovie and MovieRow components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import './App.css'
+import Tmdb from './Tmdb'
+import MovieRow from './components/MovieRow'
+import FeaturedMovie from './components/FeaturedMovie'
 
-function App() {
-  return (
-    <div className="App">
-        Netflix - clone
-    </div>
-  )
+
+export default () => {
+
+    const [movieList, setMovieList] = useState([])
+    const [featuredData, setFeaturedData] = useState(null)
+    
+    useEffect(() => {
+        const loadAll = async () => {
+            let list = await Tmdb.getHomeList()
+            setMovieList(list)
+
+            let originals = list.filter(i => i.slug === 'originals')
+            let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1))
+            let chosen = originals[0].items.results[randomChosen]
+            let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv')
+            setFeaturedData(chosenInfo)
+        }
+
+        loadAll()
+    }, [])
+
+    return (
+        <div className="page">
+            
+            {featuredData && 
+                <FeaturedMovie item={featuredData} />
+            }
+            
+            <section className="lists">
+                {movieList.map((item, key) => (
+                    <MovieRow key={key} title={item.title} items={item.items}></MovieRow>
+                ))
+
+                }
+            </section>
+        </div>
+    )
+
 }
-
-export default App
